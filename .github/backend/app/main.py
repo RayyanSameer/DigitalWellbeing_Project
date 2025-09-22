e866fd934b8e4021dbdb9f4f05d40317c2c76bfe
Rayyan Sameer <turbocharged.tomato@gmail.com>
2025-09-22 10:40:57 +0530

    Checkpoint reload

--- .github/backend/app/main.py
+++ .github/backend/app/main.py
@@ -1,7 +1,12 @@
 ﻿from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
     return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    # Runs the FastAPI app with Uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000, reload=True)

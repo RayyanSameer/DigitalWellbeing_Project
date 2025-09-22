ceb97487d26effa27962edbbd2de27f2e52fee1d
Rayyan Sameer <turbocharged.tomato@gmail.com>
2025-09-22 13:02:07 +0530

    Added CICD , Fixed DB Connection issues , Added Sequential deployment script.

--- .github/backend/app/main.py
+++ .github/backend/app/main.py
@@ -1,5 +1,16 @@
 ﻿from fastapi import FastAPI
 import uvicorn
+from dotenv import load_dotenv
+import os
+
+# Load .env file automatically (works for local dev)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Just to confirm
+print("Using database:", DATABASE_URL)
+
 
 app = FastAPI()
 
@@ -9,4 +20,4 @@ def hello():
 
 if __name__ == "__main__":
     # Runs the FastAPI app with Uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
